0fc30757dfcb0e118df18f9bd79a31dd5bc4bde0
ms29.seo <ms29.seo@gmail.com>
2016-07-03 01:12:55 +0900

    project 초기화
    spring boot + maven 으로 초기화

---
+++ src/main/java/net/brainage/wisenotes/WiseNotesApplication.java
@@ -0,0 +1,13 @@
+package net.brainage.wisenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseNotesApplication.class, args);
+    }
+
+}
